25afc912583ae59f42f6accc5e00a9a37b458492
Mariam Ali <mariamqali13@gmail.com>
2018-11-11 13:23:25 +0300

    3 features

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -9,15 +9,70 @@ const instance = axios.create({
   baseURL: "https://api-chatr.herokuapp.com/"
 });
 
-const setAuthToken = token => {};
+const setAuthToken = token => {
+  if (token) {
+    localStorage.setItem("token", token);
+    axios.defaults.headers.common.Authorization = `jwt ${token}`;
+  } else {
+    localStorage.removeItem("token");
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
 
-export const checkForExpiredToken = () => {};
+export const checkForExpiredToken = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
 
-export const login = userData => {};
+    if (token) {
+      const currentTime = Date.now() / 1000;
+      const user = jwt_decode(token);
+      console.log((user.exp - currentTime) / 60);
 
-export const signup = userData => {};
+      if (user.exp >= currentTime) {
+        setAuthToken(token);
+        dispatch(setCurrentUser(user));
+      } else {
+        dispatch(logout());
+      }
+    }
+  };
+};
 
-export const logout = () => {};
+export const login = userData => {
+  return dispatch => {
+    instance
+      .post("/login/", userData)
+      .then(res => res.data)
+      .then(user => {
+        const decodedUser = jwt_decode(user.token);
+        setAuthToken(user.token);
+        dispatch(setCurrentUser(decodedUser));
+      })
+      .catch(err => {
+        dispatch(setErrors(err.response.data));
+      });
+  };
+};
+
+export const signup = (userData, history) => {
+  return dispatch => {
+    instance
+      .post("/signup/", userData)
+      .then(res => res.data)
+      .then(user => {
+        const decodedUser = jwt_decode(user.token);
+        setAuthToken(user.token);
+        dispatch(setCurrentUser(decodedUser));
+        history.push("/");
+      })
+      .catch(err => dispatch(setErrors(err.response.data)));
+  };
+};
+
+export const logout = () => {
+  setAuthToken();
+  return setCurrentUser();
+};
 
 const setCurrentUser = user => ({
   type: actionTypes.SET_CURRENT_USER,
